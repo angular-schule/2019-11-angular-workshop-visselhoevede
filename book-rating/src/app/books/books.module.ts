6dc3abfaa7a0964853d3e2b38e5ab40b114e3664
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-11-07 10:36:28 +0100

    angular: forms

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { BookComponent } from './book/book.component';
 import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateBookComponent } from './create-book/create-book.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    BookComponent
+    BookComponent,
+    CreateBookComponent
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     BooksRoutingModule
   ],
   exports: [
