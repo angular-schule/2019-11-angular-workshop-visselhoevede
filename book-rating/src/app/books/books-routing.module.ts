537d39af727eec0dde430fa552d0fc50bf7bf11b
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-11-07 17:13:38 +0100

    first routing, but with snapshot!

--- book-rating/src/app/books/books-routing.module.ts
+++ book-rating/src/app/books/books-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'book/:isbn', component: BookDetailsComponent },
+
+  { path: '**', redirectTo: 'dashboard' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
