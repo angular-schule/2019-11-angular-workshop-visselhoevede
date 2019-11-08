5690f04e151ecb16d1151ef0e39f1a0c4a11dc95
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-11-08 11:59:38 +0100

    rxjs-playground: fromEvent

--- rxjs-playground/src/app/exercises/fromevent/fromevent.component.ts
+++ rxjs-playground/src/app/exercises/fromevent/fromevent.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { fromEvent } from 'rxjs';
-import { map, startWith, debounceTime } from 'rxjs/operators';
+import { map, startWith, debounceTime, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'rxw-fromevent',
@@ -11,10 +11,13 @@ export class FromeventComponent implements OnInit {
   currentWidth = 0;
 
   ngOnInit() {
-
     /******************************/
 
-
+    fromEvent(window, 'resize').pipe(
+      map(w => (w.target as Window).innerWidth),
+      debounceTime(100),
+      startWith(window.innerWidth),
+    ).subscribe(width => this.currentWidth = width);
 
     /******************************/
   }
