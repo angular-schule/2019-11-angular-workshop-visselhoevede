63b016f65b122410cda734b0bd2168c01538bc2f
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-11-08 15:44:51 +0100

    ngrx "boilerplate"

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -9,6 +9,10 @@ import { CreateBookComponent } from './create-book/create-book.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DatePickerModule } from '@syncfusion/ej2-angular-calendars';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { StoreModule } from '@ngrx/store';
+import * as fromBook from './reducers/book.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { BookEffects } from './effects/book.effects';
 
 @NgModule({
   declarations: [
@@ -21,7 +25,9 @@ import { BookDetailsComponent } from './book-details/book-details.component';
     CommonModule,
     ReactiveFormsModule,
     BooksRoutingModule,
-    DatePickerModule
+    DatePickerModule,
+    StoreModule.forFeature(fromBook.bookFeatureKey, fromBook.reducer),
+    EffectsModule.forFeature([BookEffects])
   ],
   exports: [
     DashboardComponent
