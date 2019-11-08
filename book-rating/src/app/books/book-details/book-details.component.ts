bf4366b0752d772721b57701a9e74d70e8ade452
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-11-08 14:53:32 +0100

    rxjs: catchError (done wrong)

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { of, from, timer, interval, Subscription, Observable } from 'rxjs';
-import { take, map, filter, reduce, mergeMap, switchMap, share, shareReplay } from 'rxjs/operators';
+import { take, map, filter, reduce, mergeMap, switchMap, share, shareReplay, catchError } from 'rxjs/operators';
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'br-book-details',
@@ -16,7 +17,14 @@ export class BookDetailsComponent {
 
   book$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
-    switchMap(isbn => this.bs.getSingle(isbn))
+    switchMap(isbn => this.bs.getSingle(isbn)),
+    catchError((e: HttpErrorResponse) => of({
+      title: 'Error load ' + e.url,
+      isbn: '00',
+      description: 'Da lief was schief!',
+      rating: 1,
+      published: undefined
+    }))
   );
 
   constructor(private route: ActivatedRoute, private bs: BookStoreService) { }
