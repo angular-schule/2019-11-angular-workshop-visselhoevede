534b99b675110d7acf899e76e92270dca197b3d4
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-11-08 14:23:06 +0100

    rxjs: shareReplay(1) vs share

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { of, from, timer, interval, Subscription, Observable } from 'rxjs';
-import { take, map, filter, reduce, mergeMap, switchMap, share } from 'rxjs/operators';
+import { take, map, filter, reduce, mergeMap, switchMap, share, shareReplay } from 'rxjs/operators';
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
 
@@ -17,7 +17,7 @@ export class BookDetailsComponent {
   book$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
     switchMap(isbn => this.bs.getSingle(isbn)),
-    share()
+    shareReplay(1)
   );
 
   constructor(private route: ActivatedRoute, private bs: BookStoreService) { }
