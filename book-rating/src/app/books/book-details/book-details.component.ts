68c857a96b83bca11331a81ec02353d6831a38d0
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-11-08 11:38:11 +0100

    rxjs-playground: creating (mit lore ipsum :-D)

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -18,25 +18,6 @@ export class BookDetailsComponent implements OnInit {
     this.route.paramMap
       .subscribe(paramMap => this.isbn = paramMap.get('isbn'));
 
-    const observer = {
-      next: data => console.log(data),
-      error: err => console.error(err),
-      complete:  () => console.log('COMPLETE! 👍')
-    };
-
-    // TODO
-    // 1. mal 10
-    // 2. nur Daten durchlassen die größer als 30
-    // 3. die Summe aus allen Zahlen
-    // 4. zeige so viele Herzen an, wie die Summe groß war (Knobeln)
-    const subscription = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).pipe(
-      map(x => x * 10),
-      filter(x => x > 30),
-      reduce((a, b) => a + b),
-      map(x => '🧡'.repeat(x))
-    ).subscribe(observer);
-
-    setTimeout(() => subscription.unsubscribe(), 2000);
   }
 
 
--- rxjs-playground/src/app/exercises/creating/creating.component.ts
+++ rxjs-playground/src/app/exercises/creating/creating.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of, from, timer, interval } from 'rxjs';
+import { take, mergeMap, concatMap, delay } from 'rxjs/operators';
 
 @Component({
   selector: 'rxw-creating',
@@ -10,11 +11,11 @@ export class CreatingComponent implements OnInit {
   myObservable$: Observable<number | string>;
 
   ngOnInit() {
-    /**
-     * Erstelle ein Observable und schreibe es in this.myObservable$
-     */
 
-
+    // tslint:disable-next-line: max-line-length
+    const loreIpsum = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+    this.myObservable$ = from(loreIpsum.split(' ')).pipe(
+      concatMap(x => of(x).pipe(delay(1000)))
+    );
   }
-
 }
